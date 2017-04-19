e9bad96bbe815a170aa07c613b3b049d8f61bc19
Marcos Paliari <marcos@paliari.com.br>
2017-04-19 19:51:12 -0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# v-tab-bar
+
+> A Vue Tab Bar Navigator
+
+# Usage
+
+## Installation
+
+### Using yarn
+
+`yarn add v-tab-bar`
+
+### Using npm
+
+`npm i --save v-tab-bar`
+
+## License
+
+This project is licensed under [MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ src/Component.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+<textarea class="v-tab-bar" v-model="editableValue" :maxlength="maxlength"></textarea>
+</template>
+
+<script>
+export default {
+  name: 'v-tab-bar',
+  props: {
+    value: {
+      type: String,
+      default: '',
+      required: true
+    },
+
+    maxlength: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+  },
+
+  data () {
+    console.log(this.value)
+    return {
+      editableValue: this.value
+    }
+  },
+
+  watch: {
+    value (newValue) {
+      this.editableValue = newValue
+    },
+
+    editableValue: {
+      immediate: true,
+      handler (newValue) {
+        this.$emit('input', newValue)
+        this.$emit('left-chars', this.leftChars)
+      }
+    }
+  },
+
+  computed: {
+    leftChars () {
+      return this.maxlength - this.editableValue.length
+    }
+  }
+}
+</script>
