bf7bbafbc59b36a0b93399d772351cac5b02497a
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-04-24 16:04:07 -0300

    fix routes for demo

--- demo/router.js
+++ demo/router.js
@@ -1,6 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+function V(name) {
+  return require(`./views/${name}.vue`)
+}
+
+let routes = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: V('Home'),
+  },
+  {
+    name: 'list',
+    path: '/list',
+    component: V('List'),
+  },
+  {
+    name: 'account',
+    path: '/account',
+    component: V('Account'),
+  },
+  {
+    name: 'config',
+    path: '/config',
+    component: V('Config'),
+  },
+  {
+    name: 'comments',
+    path: '/comments',
+    component: V('Comments'),
+  }
+]
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({ routes })
